aa160b8b0ccdea3638cefea808552b4a7555f9d4
Patrick Westphal <patrick.westphal@hitec-hamburg.de>
2024-05-03 15:25:45 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='coypu-mapping-generation',
+    version='0.0.1',
+    packages=[
+        'semanticlabeling',
+        'semanticmodeling',
+        'util',
+        'tests'
+    ],
+    scripts=[
+        'bin/infersemlabels',
+        'bin/infersemrelations'
+    ],
+    url='',
+    license='',
+    author='Patrick Westphal',
+    author_email='',
+    description='',
+    install_requires=[
+        'rdflib==7.0.0',
+        'pandas==2.2.0',
+        'matplotlib',
+        'steiner-tree==1.1.3',
+    ]
+)
