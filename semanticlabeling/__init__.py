97f0ecd5460fbcdfcc51d81adeddd62882de2c66
Patrick Westphal <patrick.westphal@hitec-hamburg.de>
2024-06-21 19:19:45 +0200

    Add column types

--- semanticlabeling/__init__.py
+++ semanticlabeling/__init__.py
@@ -5,11 +5,18 @@ from enum import Enum
 class ColumnType(Enum):
     Str = 'string'
     AtomicToken = 'atomic token'  # must not contain white spaces
+    Categories = 'categories'
     Int = 'integer'
     Real = 'real'
+    Float = 'float'
     Boolean = 'boolean'
     Byte = 'byte'
     Date = 'date'
     DateTime = 'date time'
     DateTimeStamp = 'date time stamp'
+    Text = 'text'
     Time = 'time'
+    ID = 'ID'
+    Lat = 'lat'
+    Lon = 'lon'
+    Unknown = 'unknown'
