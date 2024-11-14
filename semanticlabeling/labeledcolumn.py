d7f53c4449248547b42a3a926e81e88d4d94992e
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2024-11-14 11:45:04 +0100

    Update labeled column module

--- semanticlabeling/labeledcolumn.py
+++ semanticlabeling/labeledcolumn.py
@@ -1,4 +1,5 @@
-from abc import ABC, abstractstaticmethod, abstractmethod
+import logging
+from abc import ABC, abstractmethod
 from types import NoneType
 from typing import List, Dict, Set, Tuple, Union
 
@@ -7,13 +8,24 @@ from pandas import Series, Timestamp
 
 from semanticlabeling import ColumnType
 
+logger = logging.getLogger(__name__)
+
+
+ColumnName = str
+ResourceID = str
+
 
 class LabeledColumn(ABC):
-    def __init__(self, column_name: str):
+    def __init__(self, column_name: ColumnName):
         self.column_name = column_name
-        self.links: Dict[str, Set[LabeledColumn]] = {}
+        self.links: Dict[ColumnName, Set[LabeledColumn]] = {}
 
-    def add_link_to_other_column(self, link_name: str, target_column: 'LabeledColumn'):
+    def add_link_to_other_column(self, link_name: ColumnName, target_column: 'LabeledColumn'):
+        if target_column is None:
+            logger.warning(
+                f'target column for link {link_name} is None in column {self.column_name}'
+            )
+
         if link_name not in self.links:
             self.links[link_name] = set()
 
@@ -32,7 +44,7 @@ class LabeledColumn(ABC):
 class IDColumn(LabeledColumn):
     def __init__(
             self,
-            column_name: str,
+            column_name: ColumnName,
             min_id_length: int,
             avg_id_length: float,
             max_id_length: int
@@ -52,8 +64,8 @@ class IDColumn(LabeledColumn):
 
     def __sub__(self, other):
         if not isinstance(other, IDColumn):
-            from util.columncomparator import UncomparableException
-            raise UncomparableException
+            from util.columncomparator import IncomparableLabeledColumnException
+            raise IncomparableLabeledColumnException
         else:
             # TODO: re-check
             return abs(self.min_id_length - other.min_id_length) + \
@@ -64,10 +76,10 @@ class IDColumn(LabeledColumn):
 class UntypedIDColumn(LabeledColumn):
     def __init__(self):
         super().__init__('untyped_id_column')
-        self.entries: Set[str] = set()
-        self.entry_links: Dict[str, Set[Tuple[str, LabeledColumn]]] = dict()
+        self.entries: Set[ResourceID] = set()
+        self.entry_links: Dict[ResourceID, Set[Tuple[ColumnName, LabeledColumn]]] = dict()
 
-    def add_entry(self, id_str: str, links: Dict[str, LabeledColumn]):
+    def add_entry(self, id_str: ResourceID, links: Dict[ColumnName, LabeledColumn]):
         self.entries.add(id_str)
         entry_links = self.entry_links.get(id_str)
 
@@ -78,13 +90,17 @@ class UntypedIDColumn(LabeledColumn):
         for link_name, target in links.items():
             entry_links.add((link_name, target))
 
-    def contains_id(self, id_str: str) -> bool:
+    def contains_id(self, id_str: ResourceID) -> bool:
         return id_str in self.entries
 
-    def get_id_links(self, id_str: str) -> Union[NoneType, Set[Tuple[str, LabeledColumn]]]:
+    def get_id_links(
+            self,
+            id_str: ResourceID
+    ) -> Union[NoneType, Set[Tuple[ColumnName, LabeledColumn]]]:
+
         return self.entry_links.get(id_str)
 
-    def remove_entry(self, id_str):
+    def remove_entry(self, id_str: ResourceID):
         self.entries.remove(id_str)
         self.entry_links.pop(id_str)
 
@@ -96,17 +112,23 @@ class UntypedIDColumn(LabeledColumn):
         # generally it makes no sense to compare even two UntypedIDColumn's as
         # they are just temporary containers for things that shall go into
         # something typed
-        from util.columncomparator import UncomparableException
-        raise UncomparableException()
+        from util.columncomparator import IncomparableLabeledColumnException
+        raise IncomparableLabeledColumnException()
 
 
 class TypedIDColumn(IDColumn):
-    def __init__(self, column_name: str, min_id_length: int, avg_id_length: float, max_id_length: int):
+    def __init__(
+            self,
+            column_name: ColumnName,
+            min_id_length: int = 0,
+            avg_id_length: float = 0.0,
+            max_id_length: int = 0
+    ):
         super().__init__(column_name, min_id_length, avg_id_length, max_id_length)
-        self._ids: Set[str] = set()
+        self._ids: Set[ResourceID] = set()
         self._id_cnt = 0
 
-    def add_id(self, id_str: str):
+    def add_id(self, id_str: ResourceID):
         self._ids.add(id_str)
         self.min_id_length = min(self.min_id_length, len(id_str))
         self.max_id_length = max(self.max_id_length, len(id_str))
@@ -117,14 +139,24 @@ class TypedIDColumn(IDColumn):
             (self.avg_id_length * (self._id_cnt - 1) / self._id_cnt) + \
             (id_len / self._id_cnt)
 
-    def contains_id(self, id_str: str) -> bool:
+    def contains_id(self, id_str: ResourceID) -> bool:
         return id_str in self._ids
+
+    def update_stats(self):
+        id_lengths = list(map(lambda i: len(i), self._ids))
+        min_id_len = min(id_lengths)
+        avg_id_len = sum(id_lengths) / self._id_cnt
+        max_id_len = max(id_lengths)
+
+        self.min_id_length = min_id_len
+        self.avg_id_length = avg_id_len
+        self.max_id_length = max_id_len
 
 
 class TextColumn(LabeledColumn):
     def __init__(
             self,
-            column_name: str,
+            column_name: ColumnName,
             min_text_length: int,
             avg_text_length: float,
             max_text_length: int
@@ -154,8 +186,8 @@ class TextColumn(LabeledColumn):
 
     def __sub__(self, other):
         if not isinstance(other, TextColumn) and not isinstance(other, StringColumn):
-            from util.columncomparator import UncomparableException
-            raise UncomparableException()
+            from util.columncomparator import IncomparableLabeledColumnException
+            raise IncomparableLabeledColumnException()
 
         else:
             if isinstance(other, StringColumn):
@@ -171,7 +203,7 @@ class TextColumn(LabeledColumn):
 class StringColumn(LabeledColumn):
     def __init__(
             self,
-            column_name: str,
+            column_name: ColumnName,
             min_str_length: int,
             avg_str_length: float,
             max_str_length: int
@@ -201,8 +233,8 @@ class StringColumn(LabeledColumn):
 
     def __sub__(self, other):
         if not isinstance(other, StringColumn) and not isinstance(other, TextColumn):
-            from util.columncomparator import UncomparableException
-            raise UncomparableException()
+            from util.columncomparator import IncomparableLabeledColumnException
+            raise IncomparableLabeledColumnException()
 
         if isinstance(other, TextColumn):
             return abs(self.min_str_length - other.min_text_length) + \
@@ -215,7 +247,12 @@ class StringColumn(LabeledColumn):
 
 
 class BooleanColumn(LabeledColumn):
-    def __init__(self, column_name: str, portion_true: float, portion_false: float):
+    def __init__(
+            self,
+            column_name: ColumnName,
+            portion_true: float,
+            portion_false: float
+    ):
         super().__init__(column_name)
         self.portion_true = portion_true
         self.portion_false = portion_false
@@ -230,8 +267,8 @@ class BooleanColumn(LabeledColumn):
 
     def __sub__(self, other):
         if not isinstance(other, BooleanColumn):
-            from util.columncomparator import UncomparableException
-            raise UncomparableException()
+            from util.columncomparator import IncomparableLabeledColumnException
+            raise IncomparableLabeledColumnException()
 
         else:
             return abs(self.portion_true - other.portion_true) + \
@@ -239,7 +276,7 @@ class BooleanColumn(LabeledColumn):
 
 
 class CategoriesColumn(LabeledColumn):
-    def __init__(self, column_name: str, categories: List[str]):
+    def __init__(self, column_name: ColumnName, categories: List[str]):
         super().__init__(column_name)
         self.categories = categories
 
@@ -252,8 +289,8 @@ class CategoriesColumn(LabeledColumn):
 
     def __sub__(self, other):
         if not isinstance(other, CategoriesColumn):
-            from util.columncomparator import UncomparableException
-            raise UncomparableException
+            from util.columncomparator import IncomparableLabeledColumnException
+            raise IncomparableLabeledColumnException
 
         else:
             return 1 - len(set(self.categories).intersection(set(other.categories))) / \
@@ -263,7 +300,7 @@ class CategoriesColumn(LabeledColumn):
 class IntegerColumn(LabeledColumn):
     def __init__(
             self,
-            column_name: str,
+            column_name: ColumnName,
             min_value: int,
             avg_value: float,
             max_value: int,
@@ -285,8 +322,8 @@ class IntegerColumn(LabeledColumn):
 
     def __sub__(self, other):
         if not isinstance(other, IntegerColumn) and not isinstance(other, FloatColumn):
-            from util.columncomparator import UncomparableException
-            raise UncomparableException()
+            from util.columncomparator import IncomparableLabeledColumnException
+            raise IncomparableLabeledColumnException()
 
         else:
             return abs(self.min_value - other.min_value) + \
@@ -298,7 +335,7 @@ class IntegerColumn(LabeledColumn):
 class FloatColumn(LabeledColumn):
     def __init__(
             self,
-            column_name: str,
+            column_name: ColumnName,
             min_value: float,
             avg_value: float,
             max_value: float,
@@ -321,8 +358,8 @@ class FloatColumn(LabeledColumn):
 
     def __sub__(self, other):
         if not isinstance(other, FloatColumn) and not isinstance(other, IntegerColumn):
-            from util.columncomparator import UncomparableException
-            raise UncomparableException()
+            from util.columncomparator import IncomparableLabeledColumnException
+            raise IncomparableLabeledColumnException()
         else:
             return abs(self.min_value - other.min_value) + \
                 abs(self.avg_value - other.avg_value) + \
@@ -365,7 +402,7 @@ class WGS84LongitudeColumn(WGS84CoordinateColumn):
 class DateTimeColumn(LabeledColumn):
     def __init__(
             self,
-            column_name: str,
+            column_name: ColumnName,
             min_date_time: Timestamp,
             mean_date_time: Timestamp,
             max_date_time: Timestamp
@@ -385,17 +422,21 @@ class DateTimeColumn(LabeledColumn):
 
     def __sub__(self, other):
         if not isinstance(other, DateTimeColumn):
-            from util.columncomparator import UncomparableException
-            raise UncomparableException()
+            from util.columncomparator import IncomparableLabeledColumnException
+            raise IncomparableLabeledColumnException()
 
         else:
-            return abs(self.min_date_time.timestamp() - other.min_date_time.timestamp()) + \
-                abs(self.mean_date_time.timestamp() - other.mean_date_time.timestamp()) + \
-                abs(self.max_date_time.timestamp() - other.max_date_time.timestamp())
+            try:
+                return abs(self.min_date_time.timestamp() - other.min_date_time.timestamp()) + \
+                    abs(self.mean_date_time.timestamp() - other.mean_date_time.timestamp()) + \
+                    abs(self.max_date_time.timestamp() - other.max_date_time.timestamp())
+            except (AttributeError, ValueError):
+                import sys
+                return sys.float_info.max
 
 
 class YetUnknownTypeColumn(LabeledColumn):
-    def __init__(self, column_name: str):
+    def __init__(self, column_name: ColumnName):
         super().__init__(column_name)
         self.values = []
 
@@ -408,5 +449,5 @@ class YetUnknownTypeColumn(LabeledColumn):
 
     def __sub__(self, other):
         # comparing unknown type columns to anything does not make sense
-        from util.columncomparator import UncomparableException
-        raise UncomparableException()
+        from util.columncomparator import IncomparableLabeledColumnException
+        raise IncomparableLabeledColumnException()
