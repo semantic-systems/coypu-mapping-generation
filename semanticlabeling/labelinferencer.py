3e6f211e046245dde67072861b38f70dd6e63c51
Patrick Westphal <patrick.westphal@teebeutelstoerche.de>
2024-11-14 11:41:35 +0100

    Update label inferencer

--- semanticlabeling/labelinferencer.py
+++ semanticlabeling/labelinferencer.py
@@ -5,14 +5,12 @@ from steiner_tree.bank import BankGraph
 
 import util.graphbuilder
 from semanticlabeling.labeledcolumn import LabeledColumn
-from util.knowledgesource import KnowledgeSource
 from util.file import InputFile
 
 
 class SemanticLabelInferencer(ABC):
-    def __init__(self, input_file: InputFile, ontologies: List[KnowledgeSource]):
+    def __init__(self, input_file: InputFile):
         self.input_file = input_file
-        self.ontologies = ontologies
 
     def get_labeled_columns(self) -> List[LabeledColumn]:
         return self.input_file.columns
@@ -21,5 +19,4 @@ class SemanticLabelInferencer(ABC):
         return util.graphbuilder.build(self.input_file.columns)
 
     def print_summary(self):
-        pass  # TODO
         raise NotImplementedError()
